a32f96d32d3e0b6f45cc0194cd63cc07e2b852e7
gannaraputeja <gannaraputeja@missionrnd.com>
2016-05-19 10:45:48 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -33,10 +33,55 @@ Hint : You can use path_exists as a wrapper function for your original recursive
 more parameters .
 */
 
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 
+int safepath(int *maze, int *path, int rows, int columns, int x1, int y1, int x2, int y2)
+{
+	if (x1 >= 0 && x1 < rows && y1 >= 0 && y1 < columns && maze[x1*columns + y1] && !path[x1*columns + y1])
+		if (x2 >= 0 && x2 < rows && y2 >= 0 && y2 < columns && maze[x2*columns + y2] && !path[x2*columns + y2])
+			return 1;
+	return 0;
+}
+
+int mazepath(int *maze, int *path, int rows, int columns, int x1, int y1, int x2, int y2)
+{
+	if (x1 == x2&&y1 == y2)
+	{
+		path[x1*columns + y1] = 1;
+		return 1;
+	}
+	if (safepath(maze, path, rows, columns, x1, y1, x2, y2) == 1)
+	{
+		path[x1*columns + y1] = 1;
+		if (mazepath(maze, path, rows, columns, x1, y1 + 1, x2, y2) == 1)
+			return 1;
+		if (mazepath(maze, path, rows, columns, x1 + 1, y1, x2, y2) == 1)
+			return 1;
+		if (mazepath(maze, path, rows, columns, x1, y1 - 1, x2, y2) == 1)
+			return 1;
+		if (mazepath(maze, path, rows, columns, x1 - 1, y1, x2, y2) == 1)
+			return 1;
+		path[x1*columns + y1] = 0;
+	}
+	return 0;
+}
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+	if (rows>0 && columns>0)
+	{
+		int *path = (int*)malloc(sizeof(int)*rows*columns);
+		for (int i = 0; i<rows; i++)
+			for (int j = 0; j<columns; j++)
+				path[i*columns + j] = 0;
+		if (safepath(maze, path, rows, columns, x1, y1, x2, y2) == 1)
+		{
+			if (mazepath(maze, (int*)path, rows, columns, x1, y1, x2, y2) == 1)
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
